48754743735a0106de9974de1fd169e6e9465f9e
Nicholas Rodine <halfofastaple@gmail.com>
2020-10-30 14:29:27 -0500

    Removed old visualizer, and replaced with a console visualizer for now.

---
+++ QuantizedFluid.VisualConsole/ConsoleVisualizer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using QuantizedFluid.Core;
+
+namespace QuantizedFluid.VisualConsole {
+	public class ConsoleVisualizer {
+		private readonly FluidWorld _world;
+
+		public ConsoleVisualizer(FluidWorld world) {
+			_world = world;
+		}
+
+		public void Render() {
+			Console.WriteLine("Taking Step");
+			SetPixels();
+		}
+
+		protected void SetPixels() {
+			for (var y = 0; y < _world.Height; y++) {
+				for (var x = 0; x < _world.Width; x++) {
+					var count = _world[x, y].NumberOfParticles;
+					Console.Write($"{count}\t");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
--- QuantizedFluid.VisualConsole/Program.cs
+++ QuantizedFluid.VisualConsole/Program.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Drawing;
-using CorePixelEngine;
 using QuantizedFluid.Core;
 
-namespace QuantizedFluid {
-	public static class Program {
-		[STAThread]
+namespace QuantizedFluid.VisualConsole {
+	public class Program {
 		public static void Main() {
-			var size = new Size(20, 20);
+			var size = new Size(10, 10);
 			const int quantizations = 10;
 			var world = MakeWorld(size, quantizations);
 			world[0, size.Height / 2].VelocityProbability.X[quantizations] = 1;
 			world.NormalizeProbabilities();
 
-			var visual = new FluidWorldVisualizer(world);
-			if (visual.Construct(size.Width, size.Height, 10, 10, false, true) == RCode.OK) {
-				visual.Start();
+			var visual = new ConsoleVisualizer(world);
+			while (true) {
+				var cmd = Console.ReadLine();
+				if (cmd == "quit") return;
+				
+				world.Step();
+				visual.Render();
 			}
 		}
 
@@ -24,7 +26,7 @@ namespace QuantizedFluid {
 			for (var x = 0; x < size.Width; x++) {
 				for (var y = 0; y < size.Height; y++) {
 					var point = new Point(x, y);
-					world[point].NumberOfParticles = 127000;
+					world[point].NumberOfParticles = 10;
 				}
 			}
 			return world;
