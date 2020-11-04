cf1d8b0202939c1407d80b9a6e1dd9c64afd3ad4
Nicholas Rodine <halfofastaple@gmail.com>
2020-11-04 14:46:25 -0500

    Added + operator for two Quantization2fs.

---
+++ QuantizedFluid.Tests/Quantization2f.Tests.OperatorAdd.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using QuantizedFluid.QuantizedMath;
+
+namespace QuantizedFluid.Tests {
+	[TestFixture]
+	public class Quantization2f_Tests_OperatorAdd : Quantization2f_Tests {
+		[Test]
+		public void AbleToAddTwoQuantizations() {
+			var q1 = new Quantization2f(new[]{1.0f, 2.0f, 3.0f}, new[]{4.0f, 0.5f, 1.5f});
+			var q2 = new Quantization2f(new[]{10.0f, 11.0f, 12.0f}, new[]{.5f, 1.5f, 31});
+
+			var result = q1 + q2;
+			
+			result.GetXValues().Should().BeEquivalentTo(new[]{11f, 13, 15});
+			result.GetYValues().Should().BeEquivalentTo(new[]{4.5f, 2, 32.5f});
+		}
+	}
+}
--- QuantizedFluid/QuantizedMath/Quantization2f.Operators.cs
+++ QuantizedFluid/QuantizedMath/Quantization2f.Operators.cs
@@ -3,5 +3,10 @@
 		public static Quantization2f operator *(Quantization2f left, float right) {
 			return new Quantization2f(left.XQuantization * right, left.YQuantization * right);
 		}
+
+		public static Quantization2f operator +(Quantization2f left, Quantization2f right) {
+			return new Quantization2f(left.XQuantization + right.XQuantization,
+				left.YQuantization + right.YQuantization);
+		}
 	}
 }
